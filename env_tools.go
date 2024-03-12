4e975b14d668dbc9174e1e21f6d58d94ef156f82
Ellis Lunnon <ellislunnon@gmail.com>
2024-03-12 08:40:28 +0000

    initial commit

---
+++ env_tools.go
@@ -0,0 +1,18 @@
+package gonk
+
+import (
+	"fmt"
+	"strings"
+)
+
+func getEnvName(key string, prefix string) string {
+	replacer := strings.NewReplacer(
+		"-", "_",
+		".", "_",
+	)
+	if prefix != "" {
+		prefix = strings.ToUpper(prefix)
+		prefix = fmt.Sprintf("%s_", prefix)
+	}
+	return fmt.Sprintf("%s%s", prefix, strings.ToUpper(replacer.Replace(key)))
+}
